09d564afa69c4e71da9727b5af9659a1eef28262
Julia Zibordi <juliazibordi@tecgraf.puc-rio.br>
2025-12-02 17:00:26 -0300

    adiciona operações de editar e excluir do banco

--- app/main.py
+++ app/main.py
@@ -45,9 +45,40 @@ def create():
                                (title, author, available))
             connection.commit()
             connection.close()
-            return redirect(url_for('home'))
+            return redirect(url_for('home')) # Ou 'index'
 
     return render_template('create.html')
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    book = get_book(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        available = request.form['available']
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE books SET title = ?, author = ?, available = ?'
+                         ' WHERE id = ?',
+                         (title, author, available, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('home')) # Ou 'index'
+
+    return render_template('edit.html', book=book)
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    book = get_book(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM books WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(book['title']))
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
